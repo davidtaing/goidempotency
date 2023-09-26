311deb7b76c807186e9f703614b4be530fee6980
David Taing <adavidtaing@gmail.com>
2023-09-26 21:34:46 +1000

    feat: add gorilla/mux and migrate hello world
    to http server

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	})
+
+	http.Handle("/", r)
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/davidtaing/goidempotency
 
 go 1.21.0
+
+require github.com/gorilla/mux v1.8.0 // indirect
